e6e58c8fbbc197fee32eb5dc00d0c27d85ee5fa5
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-04-10 14:15:16 +0300

    Finished form 3.34 with hardcoded starting_remnant

--- Lab 7-8/Lab 7-8 queries.sql
+++ Lab 7-8/Lab 7-8 queries.sql
@@ -1,9 +1,31 @@
 -- Форма 3.34
-WITH invoice (group_name, total_price) AS (
-    SELECT pg.group_name,
-        SUM(rl.quantity * p.price) AS total_price FROM Receipt_lines AS rl
+WITH Incoming (group_id, total_price) AS (
+    SELECT pg.group_id,
+        ROUND(SUM(rl.quantity * p.price), 2) AS total_price FROM Receipt_lines AS rl
         JOIN Products AS p ON p.product_id = rl.product_id
         JOIN Product_groups AS pg ON pg.group_id = p.group_id
-        GROUP BY pg.group_name
-)
-SELECT * FROM invoice
+        GROUP BY pg.group_id
+),
+Outcoming(group_id, total_price) AS (
+    SELECT pg.group_id,
+        ROUND(SUM(sl.quantity * p.price), 2) AS total_price FROM Sales_lines AS sl
+            JOIN Products AS p ON p.product_id = sl.product_id
+            JOIN Product_groups AS pg ON pg.group_id = p.group_id
+    GROUP BY pg.group_id
+), semiresult_334 (group_name, starting_remnant, income, outcome, final_remnant) AS (
+    SELECT pg.group_name,
+       0.00 AS starting_remnant,
+       ISNULL(i.total_price, 0) AS income,
+       ISNULL(o.total_price, 0) AS outcome,
+       ROUND((0.00 + ISNULL(i.total_price, 0) - ISNULL(o.total_price, 0)), 2) AS final_remnant
+FROM Product_groups AS pg
+LEFT JOIN Incoming AS i ON pg.group_id = i.group_id
+LEFT JOIN Outcoming AS o ON pg.group_id = o.group_id)
+SELECT * FROM semiresult_334
+UNION
+SELECT 'Result',
+       SUM(starting_remnant),
+       SUM(income),
+       SUM(outcome),
+       SUM(final_remnant) FROM semiresult_334
+    GROUP BY group_name
